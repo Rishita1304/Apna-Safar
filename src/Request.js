2b14e6d6b6856860450b4aa5c093be8906fe2c6e
Rishita Srivastava <rixsri.13@gmail.com>
2023-08-07 17:30:01 +0530

    new changes

--- src/Request.js
+++ src/Request.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 export const publicRequest = axios.create({
-    baseURL: "http://localhost:7000/api/",
-    // baseURL: "https://travel-site-amsc.onrender.com/api/",
+    // baseURL: "http://localhost:7000/api/",
+    baseURL: "https://travel-site-amsc.onrender.com/api/",
   });
 
-  export const BASE_URL = "http://localhost:7000/api/"
-  // export const BASE_URL = "https://travel-site-amsc.onrender.com/api/"
+  // export const BASE_URL = "http://localhost:7000/api/"
+  export const BASE_URL = "https://travel-site-amsc.onrender.com/api/"
   
